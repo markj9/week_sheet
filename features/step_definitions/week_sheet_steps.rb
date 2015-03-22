824440109f4daf10a9637a63ba9b2d670ee774ef
Mark Johnson <markj9@gmail.com>
2015-03-22 18:15:02 -0400

    Second scenario is passing with hard-coded html values.

--- features/step_definitions/week_sheet_steps.rb
+++ features/step_definitions/week_sheet_steps.rb
@@ -1,6 +1,8 @@
 Given(/^there are no timesheets$/) do
 end
 
+Timesheet = Struct.new :status, :total_hours
+
 When(/^I go to the timesheets display page$/) do
   visit timesheets_url
 end
@@ -10,9 +12,17 @@ Then(/^I should see that there are no timesheets$/) do
 end
 
 Given(/^there are some timesheets$/) do
-pending # express the regexp above with the code you wish you had
+  @timesheets = [
+    Timesheet.new("draft", "40"),
+    Timesheet.new("draft", "45"),
+    Timesheet.new("approved", "42")
+  ]
 end
 
 Then(/^I should see the timesheets grouped by status$/) do
-pending # express the regexp above with the code you wish you had
+  @timesheets.each do |timesheet|
+    within(".#{timesheet.status}") do
+      page.should have_css(".timesheet", text: timesheet.total_hours)
+    end
+  end
 end
